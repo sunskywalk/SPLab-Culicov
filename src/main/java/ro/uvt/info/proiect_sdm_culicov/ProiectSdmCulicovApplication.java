36081647f2903e8d4307ffa4738fd4105829a4d6
sunskywalk <alexandru.culicov04@e-uvt.ro>
2025-01-20 20:44:05 +0200

    1st commit

---
+++ src/main/java/ro/uvt/info/proiect_sdm_culicov/ProiectSdmCulicovApplication.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.proiect_sdm_culicov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectSdmCulicovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProiectSdmCulicovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/uvt/info/proiect_sdm_culicov/ProiectSdmCulicovApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.proiect_sdm_culicov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectSdmCulicovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
